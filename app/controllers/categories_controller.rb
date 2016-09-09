41c42bbc3920f2fcf045d0a5e36ba59258a38f79
Viktoria <viktoria.lovasova@gmail.com>
2016-09-09 09:30:06 +0200

    Categories management

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,38 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories = Category.all
+    @categories = Category.all.order :name
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes category_params
+      redirect_to categories_path, notice: "Category was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      redirect_to categories_path, notice: "Category was successfulyy created."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end

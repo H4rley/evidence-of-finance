80427fc072edcf8a8eddcd1d4ff2f98080a66dd8
Viktoria <viktoria.lovasova@gmail.com>
2016-09-12 21:24:59 +0200

    Rename scopes in transaction model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,11 +2,11 @@ class Category < ActiveRecord::Base
   has_many :transactions
 
   def total_incomes(start_date, end_date)
-    transactions.for_incomes.total_sum(start_date, end_date)
+    transactions.for_inputs.total_sum(start_date, end_date)
   end
 
   def total_outcomes(start_date, end_date)
-    transactions.for_outcomes.total_sum(start_date, end_date)
+    transactions.for_outputs.total_sum(start_date, end_date)
   end
 
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,9 +7,10 @@ class Transaction < ActiveRecord::Base
   delegate :name, to: :category, prefix: true, allow_nil: true
 
   validates_numericality_of :sum
+  validates :movement, inclusion: { in: %w(input output)}
 
-  scope :for_incomes, -> { where(movement: "input")}
-  scope :for_outcomes, -> { where(movement: "output")}
+  scope :for_inputs, -> { where(movement: "input")}
+  scope :for_outputs, -> { where(movement: "output")}
   scope :total_sum, -> (start_date, end_date) {
     where("created_at >= :start_date AND created_at <= :end_date",
           start_date: start_date, end_date: end_date).sum(:sum)
